6169581ea20f002c8c086310310e405de7b38e77
Michaellooks <hj-g20005@sist.ac.jp>
2021-04-27 12:31:59 +0900

    ウィンドウの表示設定

--- main.cpp
+++ main.cpp
@@ -1,18 +1,62 @@
-#include "DxLib.h"
+//ヘッダーファイルの読み込み
+#include "DxLib.h"	//DxLibを使う時は必要
+
+//マクロ定義
+#define GAME_TITLE "ゲームタイトル"		//ゲームタイトル
+
+#define GAME_WIDTH  1280				//ゲーム画面の幅(ウィドス)
+#define GAME_HEIGHT 720					//ゲーム画面の高さ(ウィドス)
+#define GAME_COLOR	32					//ゲームの色域
+
+#define GAME_ICON_ID   333					//ゲームのアイコンのID
 
 // プログラムは WinMain から始まります
+//Windowsのプログラミング方法 = (WinAPI)で動いている！
+//DxLibは、DirectXという、ゲームプログラミングをかんたんに使える仕組み
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+
+	ChangeWindowMode(TRUE);								//ウィンドウモードに設定
+
+	SetMainWindowText(GAME_TITLE);						//ウィンドウのタイトル文字
+
+	SetGraphMode(GAME_WIDTH, GAME_HEIGHT, GAME_COLOR);	//ウィンドウの解像度を設定
+
+	SetWindowSize(GAME_WIDTH, GAME_HEIGHT);				//ウィンドウの大きさを設定
+
+	SetBackgroundColor(255, 255, 255);					//デフォルトの背景の色
+
+	SetWindowIconID(GAME_ICON_ID);
+
 	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
 	{
-		return -1;			// エラーが起きたら直ちに終了
+		return -1;				// エラーが起きたら直ちに終了
 	}
 
-	DrawPixel(320, 240, GetColor(255, 255, 255));	// 点を打つ
+	//DrawPixel(320, 240, GetColor(255, 255, 255));	// 点を打つ
 
-	WaitKey();				// キー入力待ち
+	//WaitKey();				// キー入力待ち
+
+	//無限ループ
+	while (1)
+	{
+		
+		//何かしらのキーが押されたとき
+		if (CheckHitKeyAll() != 0)
+		{
+			break;	//無限ループを抜ける
+		}
+
+		//メッセージを受け取り続ける
+		if (ProcessMessage() != 0)	//‐1のとき、エラーやウィンドウが閉じられた
+		{
+			break;	//無限ループを抜ける
+		}
+
+	}
 
 	DxLib_End();				// ＤＸライブラリ使用の終了処理
 
-	return 0;				// ソフトの終了 
+	return 0;					// ソフトの終了 
 }

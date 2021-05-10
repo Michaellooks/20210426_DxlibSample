23c3b50eb743071b3b1b56e85d7e4a17231ea24d
Michaellooks <hj-g20005@sist.ac.jp>
2021-05-10 12:28:58 +0900

    キーボードの処理のヘッダファイルをおつくりになられた

--- main.cpp
+++ main.cpp
@@ -50,85 +50,17 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	//WaitKey();				// キー入力待ち
 
-	//四角の位置を決める
-	int X = GAME_WIDTH / 2;					
-	int Y = GAME_HEIGHT / 2;
-
-	//四角の大きさを決める
-	int width = 32;				//幅
-	int height = 32;			//高さ
-
-	//円の半径を決める
-	int radius = 100;
-
-	//速度
-	int spead = 1;
-
-	int Xspead = spead;
-	int Yspead = spead;
-
 	//ダブルバッファリング有効化
 	SetDrawScreen(DX_SCREEN_BACK);
 
 	//無限ループ
 	while (1)
 	{
-		
-		//何かしらのキーが押されたとき
-		if (CheckHitKeyAll() != 0)
-		{
-			break;	//無限ループを抜ける
-		}
 
-		//メッセージを受け取り続ける
-		if (ProcessMessage() != 0)	//‐1のとき、エラーやウィンドウが閉じられた
-		{
-			break;	//無限ループを抜ける
-		}
+		if (ProcessMessage() != 0)	{ break; }	//メッセージを受け取り続ける
 
+		if (ClearDrawScreen() != 0) { break; }  //画面を消去する
 
-		//画面を消去する
-		if (ClearDrawScreen() != 0) { break; }
-
-		//四角を描画する
-		//drawbox(
-		//	x, y, x + width, y + height,
-		//	getcolor(255, 0, 0),	//色を取得
-		//	true					//塗りつぶし
-		//);
-
-		//円を描画
-		DrawCircle(
-			X, Y, radius,
-			GetColor(0, 255, 0),
-			FALSE, 5
-		);
-
-		X += Xspead;						//四角の位置を右にずらす
-		Y += Yspead;
-
-		if (Y - radius < 0 || Y + radius > GAME_HEIGHT)
-		{
-			//移動速度の符号を反転させる
-			//+1なら-1にする  /	 -1ならば+1にする
-			Yspead = -Yspead;	//移動する向きを反転
-
-			//壁に当たったら加速
-			if (Xspead > 0) { Xspead += 2; }
-			else if (Xspead < 0) { Xspead -= 2; }
-
-		}
-
-		if (X - radius < 0 || X + radius > GAME_WIDTH)
-		{
-			//移動速度の符号を反転させる
-			//+1なら-1にする  /	 -1ならば+1にする
-			Xspead = -Xspead;	//移動する向きを反転
-
-			//壁に当たったら加速
-			if (Yspead > 0) { Yspead += 2; }
-			else if (Yspead < 0) { Yspead -= 2; }
-		}
 
 
 		ScreenFlip();				//ダブルバッファリングした画面を描画
